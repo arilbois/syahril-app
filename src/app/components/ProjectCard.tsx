e057810dd5b76503cbc8088836f733616137c958
arilbois <syahrilnhh@gmail.com>
2024-10-09 00:05:24 +0700

    first commit

---
+++ src/app/components/ProjectCard.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+type ProjectCardProps = {
+  imgUrl: string;
+  title: string;
+  description: string;
+};
+
+const ProjectCard: React.FC<ProjectCardProps> = ({
+  imgUrl,
+  title,
+  description,
+}) => {
+  return (
+    <div>
+      <div
+        className="h-52 md:h-72 rounded-t-xl relative group"
+        style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}
+      >
+        {/* Content */}
+      </div>
+      <div className="text-white rounded-b-xl mt-3 bg-[#181818] py-6 px-4">
+        <h5 className="text-xl font-semibold mb-2">{title}</h5>
+        <p className="text-[#ADB7BE]">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
